fc609240599b106a4f6d5642e8dc7f3cbe13e32d
osama mohamoud <wwwhdosama@gmail.com>
2025-10-06 22:13:01 +0300

    Auto commit: 2025-10-06 22:13:00

--- Classes and Objects/intro into classes and objects.cc
+++ Classes and Objects/intro into classes and objects.cc
@@ -7,8 +7,23 @@ class clsPerson
     string FirstName;
     string LastName;
     short Age;
+    // method (function)
+    string FullName(){
+        return FirstName+ " "+LastName;
+    }
+
+public:
+    string PFirstName;
+    string PLastName;
+    short PAge;
 };
 
 int main()
 {
+    clsPerson p1;
+    // i can access these members as they are public
+    p1.PFirstName = "osama";
+    p1.PLastName = "mohamoud";
+    p1.PAge = 19;
+    //p1.FullName(); --> i couldn't access this method as it is private by default 
 }
