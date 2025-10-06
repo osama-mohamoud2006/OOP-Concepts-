ea5653326520bd9d7909195a2cfb1ff6847cec8d
osama mohamoud <wwwhdosama@gmail.com>
2025-10-06 22:08:19 +0300

    Auto commit: 2025-10-06 22:08:19

--- Classes and Objects/intro into classes and objects.cc
+++ Classes and Objects/intro into classes and objects.cc
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-
+class clsPerson
+{
+    // members ( private by default )
+    string FirstName;
+    string LastName;
+    short Age;
+};
 
 int main()
 {
-
-
-
 }
