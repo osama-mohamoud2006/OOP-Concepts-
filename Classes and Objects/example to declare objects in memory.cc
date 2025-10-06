898cbea16f4eedf6948d2d8b600f21b05619e39b
osama mohamoud <wwwhdosama@gmail.com>
2025-10-06 23:01:15 +0300

    Auto commit: 2025-10-06 23:01:14

--- Classes and Objects/example to declare objects in memory.cc
+++ Classes and Objects/example to declare objects in memory.cc
@@ -10,16 +10,22 @@ public: // make all data , method members are accessible
     string LastName;
 
     // method member
-    string FullName(){
-        return "my name is "+FirstName+" "+LastName;
+    string FullName()
+    {
+        return "my name is " + FirstName + " " + LastName;
     }
 };
 
-int main(){
+int main()
+{
 
-    clsStudent S1,S2;
-    // note : the method is shared between 2 objects 
-    S1.FirstName="Eman";
-    S1.LastName="Mansour";
-    S1.
+    clsStudent S1, S2;
+    // note : the method is shared between 2 objects
+    S1.FirstName = "Eman";   // data member ,stored new place in memory
+    S1.LastName = "Mansour"; // data member ,stored new place in memory
+    cout << S1.FullName() << endl;
+
+    S2.FirstName = "osama";   // data member ,stored new place in memory
+    S2.LastName = "mohamoud"; // data member ,stored new place in memory
+    cout << S2.FullName() << endl;
 }
