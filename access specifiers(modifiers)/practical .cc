57ecabaa835c176e5b56e09337aa722ea9c019ef
osama mohamoud <wwwhdosama@gmail.com>
2025-10-07 13:10:21 +0300

    Auto commit: 2025-10-07 13:10:20

---
+++ access specifiers(modifiers)/practical .cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class clsStudentData
+{
+private: // all members will be accessible only in this class
+    short age = 19;
+
+protected: // all members will be accessible in this class and the class which inherite this class but it cann't be accessible in int main()
+    void ShowAgeAfter10Years()
+    {
+        cout << "the student age is: " << age + 10 << endl;
+    }
+
+public: // all members here will be accessible everywhere
+    string StudentName;
+    void WelcomeTheStudent() { cout << "\n Welcome student " << StudentName << endl; }
+    void ShowDefaultAge() { cout << "the student age is: " << age << endl; }
+};
+
+int main()
+{
+    clsStudentData
+}
