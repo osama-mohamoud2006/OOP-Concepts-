49fd5c5b3de259cb16d6cefa8237fa3238126423
osama mohamoud <wwwhdosama@gmail.com>
2025-10-09 00:14:12 +0300

    Auto commit: 2025-10-09 00:14:12

--- Property set&get/practical.cc
+++ Property set&get/practical.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class clsStudent
@@ -9,6 +10,13 @@ private: // starting from now make the vars private don't make them accessible i
     short _Age = 0;
 
 public:
+
+    vector<string> GetNameChangeHistory(string Name)
+    {
+        vector<string> StoreName;
+        if(name!=" ")StoreName.push_back(name);
+    }
+
     // property set ( to set value)
     void SetId(int Id)
     {
@@ -20,19 +28,20 @@ public:
     };
     void SetName(string name)
     {
+        GetNameChangeHistory(_name); // to store old values before changing it 
         _Name = name; // changed the _Name
     }
 
     // propert get (get the values)
-    int Id()
+    int GetId()
     {
-        return _Id;
+        return _ID;
     }
-    string Name()
+    string GetName()
     {
         return _Name;
     }
-    short Age()
+    short GetAge()
     {
         return _Age;
     }
@@ -41,14 +50,16 @@ public:
 int main()
 {
     clsStudent Stu1;
-   // Stu1._Age;  --> iit is private var i cann't access it
-   /// i should use set property to assign value
+    // Stu1._Age;  --> iit is private var i cann't access it
+    /// i should use set property to assign value
 
-   // assigned values using method(set property)
-   Stu1.SetName("osama");
-   Stu1.SetId("24534");
-   Stu1.Age("19");
+    // assigned values using method(set property)
+    Stu1.SetName("osama");
+    Stu1.SetId(24533);
+    Stu1.SetAge(19);
 
-   cout<<Stu1
-
+    // get values using get property
+    cout << "Name: " << Stu1.GetName() << endl;
+    cout << "ID: " << Stu1.GetId() << endl;
+    cout << "Age: " << Stu1.GetAge() << endl;
 }
