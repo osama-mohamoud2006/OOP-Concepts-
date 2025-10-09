0a251c1bb527f352baec6cd2833cfcb722803aa0
osama mohamoud <wwwhdosama@gmail.com>
2025-10-09 11:45:13 +0300

    Auto commit: 2025-10-09 11:45:12

--- Debugging/Debugging.cpp
+++ Debugging/Debugging.cpp
@@ -1,21 +1,22 @@
 ﻿#include <iostream>
 using namespace std;
 
-class Bedan { // بضان
+class Student { 
 
 private : 
 	string _Name = "";
 	short _Age = 0;
 
 	// property get& set
+//private: // note: you cann't set these functions as private function 
 public:
-
 	void SetName(string name) { _Name = name; }	// property set
 	string GetName() { return _Name; }// property get
 
 	void SetAge(short age) { _Age = age; }
 	short GetAge() { return _Age; }
 
+public:
 	//get --> call get function when reading value
 	// put --> call set function when you modify the value
 	__declspec(property(get = GetName, put = SetName)) string Name;
@@ -25,7 +26,12 @@ public:
 
 int main() {
 
-	Bedan Student1;
-	Student1.Name = "osama";
-	cout << "Name: " << Student1.Name << endl;
+	Student Student1;
+	Student1.Name = "nagy";
+	cout << Student1.Name << endl;
+	
+	Student1.Age = 19;
+	cout << "Age: " << Student1.Age << endl;
+
+
 }
