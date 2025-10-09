229d1dfcf2854ff6d2727b0727e9b9d43062d5d2
osama mohamoud <wwwhdosama@gmail.com>
2025-10-09 11:41:33 +0300

    Auto commit: 2025-10-09 11:41:32

--- Debugging/Debugging.cpp
+++ Debugging/Debugging.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-class clsStudent {
+class Bedan { // بضان
 
 private : 
 	string _Name = "";
@@ -13,7 +13,19 @@ public:
 	void SetName(string name) { _Name = name; }	// property set
 	string GetName() { return _Name; }// property get
 
+	void SetAge(short age) { _Age = age; }
+	short GetAge() { return _Age; }
 
-
+	//get --> call get function when reading value
+	// put --> call set function when you modify the value
+	__declspec(property(get = GetName, put = SetName)) string Name;
+	__declspec(property(get = GetAge, put = SetAge)) short Age;
 
 };
+
+int main() {
+
+	Bedan Student1;
+	Student1.Name = "osama";
+	cout << "Name: " << Student1.Name << endl;
+}
