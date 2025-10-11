b63e9699addc51bcc381f1db8de1c89433123b7c
osama mohamoud <wwwhdosama@gmail.com>
2025-10-11 22:53:13 +0300

    Auto commit: 2025-10-11 22:53:12

--- Classes and Objects/Encapsulation.cc
+++ Classes and Objects/Encapsulation.cc
@@ -4,3 +4,10 @@
 
 // encapsulation is achieving data hiding but itself isn't hiding
 
+class clCpasule1{
+    private : // private data/method members can be accessed only in this class
+    int x =0; 
+    int y=0;
+
+    public: 
+};
