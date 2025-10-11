d7cc728e84bb48e7777eb1731692ca15c29c3bff
osama mohamoud <wwwhdosama@gmail.com>
2025-10-11 22:55:05 +0300

    Auto commit: 2025-10-11 22:55:05

--- Classes and Objects/Encapsulation.cc
+++ Classes and Objects/Encapsulation.cc
@@ -1,13 +1,25 @@
-// all vars and methods into class 
+// all vars and methods into class
 
 // capusale have --> vars , methods
 
 // encapsulation is achieving data hiding but itself isn't hiding
+#include<iostream>
+using namespace std; 
 
-class clCpasule1{
-    private : // private data/method members can be accessed only in this class
-    int x =0; 
-    int y=0;
+class clCpasule1
+{
+private: // private data/method members can be accessed only in this class
+    int x = 0;
+    int y = 0;
+    int input(string message)
+    {
+        cout<<"\n"<<message<<endl;
+        cin>>
+    }
 
-    public: 
+public: // public data/method members can be accessed  in this class & and outside class
+    // get property
+    void GetX()
+    {
+    }
 };
