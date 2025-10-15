6a8c9d82cc630ac6cc911100856cca3d9a523e89
osama mohamoud <wwwhdosama@gmail.com>
2025-10-15 22:25:36 +0300

    Auto commit: 2025-10-15 22:25:36

--- Constructors & Destructors/Constructors.cc
+++ Constructors & Destructors/Constructors.cc
@@ -4,12 +4,32 @@ using namespace std;
 class clsTest
 {
 private:
-string _name;
+    string _name;
+
+public: // conustractor is created by compiler by default , it always calling after obj is created and it is public (and should be public if you created it )
+    // conustractor
     clsTest() // doesn't have data type , its name is the same as class name
-    { 
+    {
+    }
+
+public:
+    void SetName(string name)
+    {
+        _name = name;
+    }
+    private:
+    string GetName()
+    {
+        return _name ;
+    }
+public:
+    void PrintName(){
+        cout<<"the name is: "<<GetName()<<endl;
     }
 };
 
-int main(){
-    
+int main()
+{
+    clsTest t1;
+    t1.PrintName();
 }
