f927563e063e075051400c0bb0f6aa54e9492910
osama mohamoud <wwwhdosama@gmail.com>
2025-10-15 22:54:43 +0300

    Auto commit: 2025-10-15 22:54:42

--- Constructors & Destructors/Constructors.cc
+++ Constructors & Destructors/Constructors.cc
@@ -9,9 +9,11 @@ private:
 
 public: // constructor is function  created by compiler by default , it always calling after obj is created and it is public (and should be public if you created it )
     // constructor
-    clsTest() // doesn't have data type , its name is the same as class name
+    clsTest(string name, int age ) // doesn't have data type , its name is the same as class name
     {
         cout<<"\nCARD DATA\n";
+        _age = age; 
+        _name = name;
     }
 
 public:
@@ -46,7 +48,7 @@ public:
 
 int main()
 {
-    clsTest t1; // i should assign value in the constructor (even i initlized the values)
-   // t1.SetName("osama");
+    clsTest t1("osa",19); // i should assign value in the constructor (even i initlized the values)
+    t1.SetName("osama");
     t1.Print();
 }
