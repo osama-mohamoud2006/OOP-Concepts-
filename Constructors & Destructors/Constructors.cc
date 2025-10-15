fbbccfc132a657381e6fbaa612d10921ef31c9ed
osama mohamoud <wwwhdosama@gmail.com>
2025-10-15 22:16:45 +0300

    Auto commit: 2025-10-15 22:16:45

--- Constructors & Destructors/Constructors.cc
+++ Constructors & Destructors/Constructors.cc
@@ -1,3 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+class clsTest
+{
+private:
+string _name;
+    clsTest() // doesn't have data type , its name is the same as class name
+    { 
+    }
+};
+
+int main(){
+    
+}
