cb0a211e07d542388f0e71b376d0f301f8e2920d
osama mohamoud <wwwhdosama@gmail.com>
2025-10-16 12:33:39 +0300

    Auto commit: 2025-10-16 12:33:38

--- Constructors & Destructors/rev on dynamic memory allocation.cc
+++ Constructors & Destructors/rev on dynamic memory allocation.cc
@@ -3,9 +3,18 @@ using namespace std;
 
 int main()
 {
-    int x =10; 
+    int x = 10;
     int *ptr = &x;
-    cout<<ptr<<endl; // will print the address of x 
-    cout<<
+    cout << ptr << endl;  // will print the address of x
+    cout << &ptr << endl; // will print the address of pointer itself
 
+    // dynamic memory allocation
+    int *ptr2 = new int; // allocated int in heap memory
+
+    *ptr2 = 190; // * --> derefrencing , assigned value to this int
+
+    cout << *ptr2 << endl;
+
+    delete ptr2;
+     cout << *ptr2 << endl;
 }
