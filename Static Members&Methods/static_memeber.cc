c060a703a27c1ba687cbba678645ba63d2e713aa
osama mohamoud <wwwhdosama@gmail.com>
2025-10-18 23:21:26 +0300

    Auto commit: 2025-10-18 23:21:26

--- Static Members&Methods/static_memeber.cc
+++ Static Members&Methods/static_memeber.cc
@@ -12,18 +12,36 @@ using namespace std;
 
 class clsA
 {
-private:
+private: //static member can be private and can be public 
     // static int _x=0; // you cann't initialize the static data member in class
 
     static int _x; // static member
 public:
     clsA() // constructor (must be public)
     {
-        _x = 0;
+        //_x = 0;// //you cann't initialize the static data member in class constructor
+        _x++;
+    }
+
+public:
+    void print()
+    {
+        cout << _x << endl;
     }
 };
+//*Note: you cann't use static member without initilization
+int clsA::_x = 0; //----> intilization must be outside class and before main
 
 int main()
 {
-    clsA x1;
+    clsA x1, x2, x3, x4, x5;
+    // x1.print();//
+    // x1.print();//
+    // x1.print();//
+    x1.print(); // will print the same value as static member changes from one object to another not the object
+
+    x2.print();
+    x3.print();
+    x4.print();
+    x5.print();
 }
