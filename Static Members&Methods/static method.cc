941b1a695e000c25ee3fe7c36081eb5533433b0e
osama mohamoud <wwwhdosama@gmail.com>
2025-10-19 22:55:56 +0300

    Auto commit: 2025-10-19 22:55:56

--- Static Members&Methods/static method.cc
+++ Static Members&Methods/static method.cc
@@ -7,13 +7,21 @@ private:
     int _x;         // not-static data member
     static int _Sx; // static data member
 public:
-    static void AddOne() // you cann't access this method if it is private 
+static int AddOneToX(){
+  //  return ++_x;// // you cann't use static data member with static method 
+}
+    static int AddOne() // you cann't access this method if it is private
     {
-        _Sx++; // it is static data member
+        return ++_Sx; // it is static data member
     }
 };
 
+int clsM::_Sx=0; // you should initlize static var before main and outside class
+
 int main()
 {
-    clsM::AddOne();
+    cout << clsM::AddOne() << endl; // i could call the static method without make any objects
+
+    clsM ex1;
+    cout<<ex1.AddOne()<<endl; // i could call the static method with objects normally
 }
