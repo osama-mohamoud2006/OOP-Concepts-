bff9d5750c14aa0d6048f35f5b8cc1bb271eea61
osama mohamoud <wwwhdosama@gmail.com>
2025-10-21 13:42:07 +0300

    Auto commit: 2025-10-21 13:42:07

--- Inheritance/Parameterized Constructor of the Base Class.cc
+++ Inheritance/Parameterized Constructor of the Base Class.cc
@@ -107,8 +107,19 @@ private:
 
 public:
     // sub-class(all arguments that assigned as parametrs in super class ):super-class(the name of that parameters )
-    clsEmployee(int id, string Fname, string lname, string email, string phone) : clsPerson(id, Fname, lname, email, phone) // sent the parameters to the paramertized constructor (in super class)
+
+    clsEmployee(int id, string Fname, string lname,
+         string email, string phone, // all of them are inherited from super class (clsPerson)
+
+        string Department , string title , int salary) 
+
+        : clsPerson(id, Fname, lname, email, phone) // sent the parameters to the paramertized constructor (in super class)
     {
+        //intilize the clsEmployee data members
+        _Department=Department;
+        _Title = title;
+        _Salary = salary;
+
     }
 
     // property get
@@ -142,14 +153,14 @@ public:
     }
 };
 
-class clsDoctors : public clsEmployee
+class clsManagers : public clsEmployee
 { // class inherited clsEmployee
   // it will inherit the clsPerson(From clsEmployee (sub class of clsPeron)) and clsEmployee
 };
 
 int main()
 {
-    // Note : values that assigned to data members in super class won't be inheritned to the sub class
+    // Note : values that assigned to data members in super class won't be inherited to the sub class
 
     clsPerson p1(10, "osama", "moahmoud", "weui@ol.com", "+2129023109"); // parametrized constructor in super class
     p1.PrintFullName();
@@ -159,7 +170,8 @@ int main()
     //  i didn't make parametrized constructor in derived class(the default constructor of "clsEmployee" cannot be referenced -- it is a deleted function)
 
     // initilzed the super class successfully(cuz i inherited it , and i used parametrized constructor in super class,so i should initialize the super class first )
-    clsEmployee emp1(10, "E/jack", "olo", "wyhyu@pop.com", "+2982394");
+    clsEmployee emp1(10, "E/jack", "olo", "wyhyu@pop.com", "+2982394","Sofware","Software Enginnering",1293);
 
     cout << "\nFull name of employee is: " << emp1.PrintFullName() << endl;
+    cout<<"the deparment is: "<<emp1.GetDepartment()<<endl;
 }
