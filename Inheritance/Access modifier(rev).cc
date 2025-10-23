0960616ae0b024989f6ab65667ecc7930cff6fb8
osama mohamoud <wwwhdosama@gmail.com>
2025-10-24 01:11:20 +0300

    Auto commit: 2025-10-24 01:11:20

--- Inheritance/Access modifier(rev).cc
+++ Inheritance/Access modifier(rev).cc
@@ -9,6 +9,12 @@ private: // accessible within class only
     string _ID;
 
 public: // accessible everywhere
+    clsPersonInfo(string Fname, string Lname, string ID)
+    { // parametrized constructor
+        _FirstName = Fname;
+        _LastName = Lname;
+        _ID = ID;
+    }
     // set
     void SetFirstName(string FName)
     {
@@ -37,7 +43,31 @@ protected: // accessible within the sub classes only
     }
 };
 
-class clsPrintInfo
+class clsPrintInfo : public clsPersonInfo
 {
-    
+    // i set  property Get as protected access modifier
+public:
+    clsPrintInfo(string fname, string lname, string id)
+        : clsPersonInfo(fname, lname, id)
+    {
+    }
+
+    void Print()
+    {
+        /*
+private: // I couldn't access them as they are private data members
+    string _FirstName;
+    string _LastName;
+    string _ID;
+
+        */
+        // clsPersonInfo:: You can only access public & protected member methods and data members
+
+        // get full name, get id
+        // is protected (access specifiers) i could acsess them here as i use them
+        // in  inherited class
+
+        cout << "The full name is: " << GetFullName() << endl;
+        cout << "The Id is: " << GetID() << endl;
+    }
 };
