f950197f571f62151816f8ab9feb1a4ae3edb76d
osama mohamoud <wwwhdosama@gmail.com>
2025-10-25 23:47:35 +0300

    Auto commit: 2025-10-25 23:47:35

--- Inheritance/inheritance visability modes.cc
+++ Inheritance/inheritance visability modes.cc
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class  clsPersonInfo
+// Super
+class clsPersonInfo
 {
 private: // accessible within class only
     string _FirstName;
@@ -42,3 +43,56 @@ protected: // accessible within the sub classes only
         return _ID;
     }
 };
+
+class clsEmployee : public clsPersonInfo
+{
+    // i inherited the base class publicly
+    //  this means : 1- The dervied class from this class will access what i inherited from my
+    //  super class (clsPersonInfo)
+    //  2- I can access what this class inherited in int main()
+
+    // i am derived class from clsPersonInfo --> i can access protected and public data members
+
+private:
+    string _Department;
+    string _Salary;
+
+public:
+    // constructor
+    clsEmployee(string Fname, string Lname, string ID,
+                string department, string salary) // sub class
+        : clsPersonInfo(Fname, Lname, ID)         // base class
+    {
+        _Department = department;
+        _Salary = salary;
+    }
+
+    void SetDepartment(string Department)
+    {
+        _Department = Department;
+    }
+
+    void SetSalary(string salary) { _Salary = salary; }
+
+    string GetSalary() { return _Salary; }
+    string GetDepartment()
+    {
+        return _Department;
+    }
+
+    void print(){
+        cout<<"The full name is: "<<GetFullName()<<endl; // this is protercted data member
+    }
+};
+
+int main()
+{
+    /* From Super CLass (clsPersonInfo)
+    string _FirstName;
+    string _LastName;
+    string _ID;
+    */
+
+    clsEmployee emp1;
+    emp1.
+}
