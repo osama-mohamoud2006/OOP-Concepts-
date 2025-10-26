5a780bd971bd407ff8c9ca1e1b5c5b1e99c97ec7
osama mohamoud <wwwhdosama@gmail.com>
2025-10-26 23:44:47 +0300

    Auto commit: 2025-10-26 23:44:46

--- Inheritance/Inheritance Types.cc
+++ Inheritance/Inheritance Types.cc
@@ -1,7 +1,47 @@
 #include <iostream>
 using namespace std;
 
-// single inheritance
+// Super class
 class clsPerson
 {
+private:
+    string _firstname;
+    string _lastname;
+
+public:
+    void SetFirstName(string first)
+    {
+        _firstname = first;
+    }
+    void setLastname(string last)
+    {
+        _lastname = last;
+    }
+
+    string GetFirstname()
+    {
+        return _firstname;
+    }
+
+    string GetLastname()
+    {
+        return _lastname;
+    }
+};
+
+// single inheritance
+class clsPeople : public clsPerson
+{
+
+public:
+    clsPeople()
+    {
+        SetFirstName("ahmed");
+        setLastname("Ali");
+    }
+
+    string SinglePersonName()
+    {
+        return GetFirstname() + " " + GetLastname();
+    }
 };
