c68acfb1506bf87ec9b7507f8a93728a3dd13ec9
osama mohamoud <wwwhdosama@gmail.com>
2025-10-27 21:55:41 +0300

    Auto commit: 2025-10-27 21:55:41

--- Inheritance/Inheritance Types.cc
+++ Inheritance/Inheritance Types.cc
@@ -60,8 +60,21 @@ public:
 
 // hierichal inheritance
 
+class A : public clsPerson
+{
+};
 
+class B : public clsPerson
+{
+};
 
+class C : private clsPerson
+{
+};
+
+class D : public clsPerson
+{
+};
 
 int main()
 {
@@ -70,9 +83,15 @@ int main()
 
     clsMPeople m;
     m.SinglePersonName(); // public property in clsPeople,so i could i access it
-   // m.SetFirstName("");// inaccessible(from super class and the inheritance visibility mode is private so i couldn't access it)
+    // m.SetFirstName("");// inaccessible(from super class and the inheritance visibility mode is private so i couldn't access it)
 
-   m.Test();
+    m.Test();
 
+    D d1; //inheritance visiblity mode is public//
+    d1.SetFirstName("Gaga");
+    cout<<d1.GetFirstname()<<endl; 
+
+    C c1; //inheritance visiblity mode is private//
+    //c1.SetFirstName("sasa");//
 
 }
