c67dbe01fbd231057c1a83d766df3a3d2195dd1e
osama mohamoud <wwwhdosama@gmail.com>
2025-10-27 21:41:19 +0300

    Auto commit: 2025-10-27 21:41:16

--- Inheritance/Inheritance Types.cc
+++ Inheritance/Inheritance Types.cc
@@ -34,7 +34,7 @@ class clsPeople : private clsPerson
 {
 
 public:
-     clsPeople() // you should use constructors to assign values before return values
+    clsPeople() // you should use constructors to assign values before return values
     {
         SetFirstName("ahmed");
         setLastname("Ali");
@@ -42,17 +42,34 @@ public:
 
     string SinglePersonName()
     {
-        
-       return (GetFirstname() + " " + GetLastname());
 
-        
+        return (GetFirstname() + " " + GetLastname());
     }
 };
+
+// multi-level inheritance
+class clsMPeople : public clsPeople
+{
+
+public:
+    void Test()
+    {
+        cout << "Hello World!\n";
+    }
+};
+
+
 
 int main()
 {
     clsPeople p1;
     cout << p1.SinglePersonName() << endl;
+
+    clsMPeople m;
+    m.SinglePersonName(); // public property in clsPeople,so i could i access it
+   // m.SetFirstName("");// inaccessible(from super class and the inheritance visibility mode is private so i couldn't access it)
+
+   m.Test();
 
 
 }
