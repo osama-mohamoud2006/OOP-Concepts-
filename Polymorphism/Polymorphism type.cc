1c8974ca9ddecd480a1a000bdf495d9e3f8d48d8
osama mohamoud <wwwhdosama@gmail.com>
2025-10-30 23:50:27 +0200

    Auto commit: 2025-10-30 23:50:27

--- Polymorphism/Polymorphism type.cc
+++ Polymorphism/Polymorphism type.cc
@@ -1,6 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Polymorphism --> function overloding , function overriding , operator overloading ,virtual function
-   
-// function overloading 
+
+// function overloading
+// you should name all functions with the same name , but you must care about the data type of parameters, idc about what function return or what is the function doing
+void print()
+{
+    cout << "Hi" << endl;
+}
+void print(string name)
+{
+    cout << "Hi " << name << endl;
+}
+string print(short age)
+{
+    cout << "the age is: " << age << endl;
+}
+
+int main()
+{
+    print();
+    print("osama");
+    cout << print(19);
+}
