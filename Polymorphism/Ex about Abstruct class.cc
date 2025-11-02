de21f2912a2a425ee6ee67766825df0a6f9c48df
osama mohamoud <wwwhdosama@gmail.com>
2025-11-02 19:53:34 +0200

    Auto commit: 2025-11-02 19:53:34

--- Polymorphism/Ex about Abstruct class.cc
+++ Polymorphism/Ex about Abstruct class.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+// Abstruct class , Contract
 class clsProperites
 {
     // set
@@ -10,17 +10,39 @@ class clsProperites
     virtual string GetId() = 0;
     virtual string GetUsername() = 0;
 };
+
 // suppose it is college system
 class clsDoctor : clsProperites
 {
-    private :
+private:
     string _DepartName;
     double _Salary;
     string _id;
+    string _user;
 
-    public:
-    void SetId() override{
-        
+public:
+    void SetId(string id) override
+    {
+        _id = id;
     }
 
+    void SetUsername(string user) override
+    {
+        _user = user;
+    }
+
+    string GetId()
+    {
+        return _id;
+    }
+    string GetUsername()
+    {
+        return _user;
+    }
+
+    void SetSalart(double sum)
+    {
+        _Salary = sum;
+    }
+    void 
 };
