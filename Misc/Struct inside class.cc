365bce20880590b3a120080928163f2c81a27ee5
osama mohamoud <wwwhdosama@gmail.com>
2025-11-03 22:56:41 +0200

    Auto commit: 2025-11-03 22:56:41

--- Misc/Struct inside class.cc
+++ Misc/Struct inside class.cc
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
-// Abstract class ,Contract 
+// Abstract class ,Contract
 class clsPropForPerson
 {
     // pure virtual function
     virtual void SetFirstName(string Fname) = 0;
     virtual void SetLastName(string Lname) = 0;
-    virtual void SetAge(short age)=0;
-
+    virtual void SetAge(short age) = 0;
 };
-class clsPersonData:clsPropForPerson
+class clsPersonData : clsPropForPerson
 {
-    private:
-    struct stPersonData{
-        string firsn
+private:
+    struct _stPersonData
+    {
+        string firstname = "";
+        string lastname = "";
+        short age = 0;
     };
+    _stPersonData d;
+
+public:
+    // set
+    void SetFirstName(string fname)
+    {
+        d.firstname = fname;
+    }
 };
