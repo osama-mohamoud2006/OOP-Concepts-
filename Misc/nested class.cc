953c7f9ee3f6b73a540b3d2e136ca35624cf9f18
osama mohamoud <wwwhdosama@gmail.com>
2025-11-04 00:00:37 +0200

    Auto commit: 2025-11-04 00:00:37

--- Misc/nested class.cc
+++ Misc/nested class.cc
@@ -4,3 +4,32 @@
 using namespace AllStuff;
 using namespace std;
 
+// inclusion class
+class clsPerson
+{
+private:
+    class clsAdderss
+    {
+    private:
+        int _StreetNum;
+        string _StreerName;
+
+    public:
+        clsAdderss(int strnum, string strname)
+        {
+            _StreetNum = strnum;
+            _StreerName = strname;
+        }
+        void SetStreetNum(int num)
+        {
+            _StreetNum = num;
+        }
+        void SetStreerName(string name)
+        {
+            _StreerName = name;
+        }
+        string StreetName(){
+
+        }
+    };
+};
