1a8f979cf6a7fbc4ce85e4c71f4f546a50e0c5f5
osama mohamoud <wwwhdosama@gmail.com>
2025-11-05 16:26:09 +0200

    Auto commit: 2025-11-05 16:26:08

---
+++ Misc/sperate class in lib/Main.c++
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"clsPerson.h";
+using namespace std;
+
+int main(){
+    
+    clsPerson p1();
+
+}
---
+++ Misc/sperate class in lib/clsPerson.h
@@ -0,0 +1,90 @@
+#pragma once // --> to avoid compiler erros when include the .h file
+#include<iostream>
+#include<string>
+using namespace std;
+
+class clsPerson
+{
+private:
+    string _FirsName;
+    string _LastName;
+    short _Age;
+    class clsAddress
+    {
+    private:
+        int _StreetNum;
+        string _StreetName;
+
+    public:
+        // parameterized constructor
+        clsAddress(int strnum, string strname)
+        {
+            _StreetNum = strnum;
+            _StreetName = strname;
+        }
+        void SetStreetNum(int num)
+        {
+            _StreetNum = num;
+        }
+        void SetStreerName(string name)
+        {
+            _StreetName = name;
+        }
+        string StreetName()
+        {
+            return _StreetName;
+        }
+        int StreetNum()
+        {
+            return _StreetNum;
+        }
+
+        void PrintStreetData()
+        {
+            cout << "the street is: " << to_string(_StreetNum) + "-" + _StreetName << endl;
+        }
+    };
+
+public:
+    clsAddress address = clsAddress(0, ""); // Initial the values of constructor (from inner class)
+    // class name obj = class name(your constructor parameters)
+
+    clsPerson(string fname, string lname, short age, int StreetNum, string StreetName)
+    {
+        address = clsAddress(StreetNum, StreetName); // assign values to parameters of clsAdderss class
+        _FirsName = fname;
+        _LastName = lname;
+        _Age = age;
+    }
+
+    void SetFname(string fname)
+    {
+        _FirsName = fname;
+    }
+
+    void SetLname(string lname)
+    {
+        _LastName = lname;
+    }
+
+    void SetAge(short age)
+    {
+        _Age = age;
+    }
+
+    string GetFullName()
+    {
+        return _FirsName + " " + _LastName;
+    }
+    short GetAge()
+    {
+        return _Age;
+    }
+
+    void PrintPersonData()
+    {
+        cout << "the full name is: " << GetFullName() << endl;
+        cout << "the age is: " << GetAge() << endl;
+    }
+};
+
