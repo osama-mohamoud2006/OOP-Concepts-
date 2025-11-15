e8b7b7bdfee11983e9e86f22623abd9f79132a3d
osama mohamoud <wwwhdosama@gmail.com>
2025-11-15 21:30:37 +0200

    Auto commit: 2025-11-15 21:30:36

---
+++ Misc/'this'.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class clsPerson{
+    private:
+    string _FirstName ;
+    string _lastName;
+    static int count ;
+
+};
+int clsPerson::count =0; // initlize the static member 
+int main(){
+
+}
--- Misc/sperate class in lib/Main.c++
+++ Misc/sperate class in lib/Main.c++
@@ -1,5 +1,5 @@
 #include<iostream>
-#include"clsPerson.h";
+#include"clsPerson.h"
 using namespace std;
 
 int main(){
