29d6482e385ef9b14cd1879b288d144ebc999a9d
osama mohamoud <wwwhdosama@gmail.com>
2025-11-15 22:31:59 +0200

    Auto commit: 2025-11-15 22:31:58

--- Misc/this Pointer .cc
+++ Misc/this Pointer .cc
@@ -12,7 +12,7 @@ public:
     clsPerson(string FirstName, string LastName)
     {
         this->FirstName = FirstName; // if you wrote the same name
-        _lastName = LastName;// compiler will implement 'this' anyway
+        _lastName = LastName;        // compiler will implement 'this' anyway
     }
     // s 1
 
@@ -21,19 +21,36 @@ public:
     void GetAllData()
     {
         cout << "First name: " << this->FirstName << endl; // you can use this-> poiner
-        cout << "Last name: " << _lastName << endl;     // or compiler will use it implicitly
+        cout << "Last name: " << _lastName << endl;        // or compiler will use it implicitly
         cout << "count: " << count << endl;
     }
 
     // s2
-    //satic method isn't belonging to any object (on the same class level)
+    // satic method isn't belonging to any object (on the same class level)
     // it is shared between all objects (on the same class level)
     // static method doesn't have this pointer , so it cann't access non-static data members
 
     static void GetPersonDetails()
     {
-      
-       
+        // cuz it is static function
+        // so i couldn't access non-static data members
+        // so in the static method you should make new obj or use static members only
+        clsPerson p2("Mohammed", "Ahmed");
+        cout << p2.FirstName << endl; // non static
+        cout << ++count << endl;      // static member ,'this' may only be used inside a nonstatic member function
+    }
+
+    static void GetPersonDetails(clsPerson PassThisOFObject)
+    {
+        PassThisOFObject.GetAllData(); // non static method 
+    }
+    void CallGetPersonDetails()
+    {
+        // GetPersonDetails() --> is static method , so it doesn't have 'this' pointer
+        // so if you want to make static method access the non-static data members
+        // you should send '*this' as a parameter
+
+        GetPersonDetails(*this); // without it the static method will create new obj of 'PassThisOFObject' with new data , but with 'this' i will access the non static data members in the object 
     }
 
     // s3
@@ -54,6 +71,5 @@ int clsPerson::count = 0; // initlize the static member
 int main()
 {
     clsPerson person1("osama", "mohamoud");
-    
-
+    person1.CallGetPersonDetails();
 }
