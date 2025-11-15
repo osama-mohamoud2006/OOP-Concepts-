d5a77066ec60129f7b7332ee54a10004f109d587
osama mohamoud <wwwhdosama@gmail.com>
2025-11-16 00:33:13 +0200

    Auto commit: 2025-11-16 00:33:12

--- Hello Pointers/Passing Objects to Functions (ByRef/ByVal).cc
+++ Hello Pointers/Passing Objects to Functions (ByRef/ByVal).cc
@@ -27,21 +27,24 @@ public:
 
 int main()
 {
-    /// clsX x1;
-    // int n=0;
-    //
+    clsX x1;
+    int n = 0;
+
     // call by ref
-    // cout << "Before using funtion: " << n<< endl;//0
-    // x1.NewS(n);
-    // cout << "After using function: "<<n<<endl; //200
+    cout << "Before using funtion: " << n << endl; // 0
+    x1.NewS(n);
+    cout << "After using function: " << n << endl; // 200
 
     // call by val
     clsX x2;
     x2.s = 10;
+
     x2.ClassByVal(x2);
     cout << "the s val is(by value): " << x2.s << endl; // 10
 
     // call by  ref
     x2.ClassByRef(x2);
     cout << "the s val is(by ref): " << x2.s << endl; // 1000
+
+   
 }
