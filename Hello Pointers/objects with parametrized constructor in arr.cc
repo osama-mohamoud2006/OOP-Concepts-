7e4bd409491d5adad9bafe6be31107888bf567d1
osama mohamoud <wwwhdosama@gmail.com>
2025-11-16 16:49:21 +0200

    Auto commit: 2025-11-16 16:49:21

--- Hello Pointers/objects with parametrized constructor in arr.cc
+++ Hello Pointers/objects with parametrized constructor in arr.cc
@@ -18,6 +18,7 @@ public:
 
 int main(){
     //static array 
+    // intializing 3 objects using parameterized constructor
     clsP arr[]={clsP(10),clsP(20),clsP(30)}; // pass value in parameterized constructor 
     for(clsP i:arr){
         i.Print();
