481105f7cc51b80dab2414c5e9e9aa032db90443
osama mohamoud <wwwhdosama@gmail.com>
2025-11-16 16:35:12 +0200

    Auto commit: 2025-11-16 16:35:11

---
+++ Hello Pointers/objects with parametrized constructor in arr.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class clsP
+{
+    int x;
+
+public:
+    clsP(int value)
+    {
+        this->x = value;
+    }
+
+    void Print(){
+        cout<<x;
+    }
+};
+
+int main(){
+    //static array 
+    clsP arr[]={clsP(10),clsP(20),clsP(30)}; // pass value in parameterized constructor 
+    for(clsP i:arr){
+        i.Print();
+        cout<<" ";
+    }
+}
